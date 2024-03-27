06277e6c88f3a8a7be0b21819bfd81f20685d4f5
Mekafuyu <mayconbrunob@hotmail.com>
2024-03-27 15:32:06 -0300

    web and model improvment

--- Server/ml_app/ml.py
+++ Server/ml_app/ml.py
@@ -3,6 +3,27 @@ from flask import (
 )
 bp = Blueprint('ml', __name__, url_prefix='/ml')
 
-@bp.route('/hello', methods=('GET', 'POST'))
-def register():
+@bp.route('/', methods=('GET', 'POST'))
+def predict():
+    if request.method == 'POST':
+        cut = request.form['cut']
+        carat = request.form['carat']
+        error = None
+
+        if not cut:
+            error = 'Cut is required.'
+        elif not carat:
+            error = 'Carat is required.'
+
+        if error is None:
+            print("ok")
+        else:
+            return redirect(url_for("auth.login", ))
+
+        flash(error)
+
     return render_template('index.html')
+
+@bp.route('/results')
+def results():
+    return "hello"
